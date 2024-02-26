aadc1a44273e77caf7b05327475c6140ca6bf18d
mohamedreda0 <prindermedo8@gmail.com>
2024-02-26 04:04:06 +0200

    add cats

---
+++ frontend/src/app/cat-5/cat-5.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-5',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-5.component.html',
+  styleUrl: './cat-5.component.css'
+})
+export class Cat5Component {
+
+}
---
+++ frontend/src/app/random-product/random-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-random-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './random-product.component.html',
+  styleUrl: './random-product.component.css'
+})
+export class RandomProductComponent {
+
+}
