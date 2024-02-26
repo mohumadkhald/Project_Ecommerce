fd9f3442732a549e85e57affab0804489318f44a
mohamedreda0 <prindermedo8@gmail.com>
2024-02-26 04:01:38 +0200

    add side bar and its sliders

---
+++ frontend/src/app/side-bar-sliders/side-bar-sliders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-sliders',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar-sliders.component.html',
+  styleUrl: './side-bar-sliders.component.css'
+})
+export class SideBarSlidersComponent {
+
+}
