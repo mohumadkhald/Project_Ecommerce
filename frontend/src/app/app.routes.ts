449ba3b0628748e7fc27091dadbf5504a51af227
mohumadkhald <mohumadkhald@gmail.com>
2024-02-27 16:50:31 +0200

    not found component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './compon_mk/profile/profile.component';
-// import { NotFoundComponent } from './compon_mk/not_found /not-found.component';
 import { MainPageComponent } from './components/mu-reda/main_page/main-page.component';
 import { PageDetailsComponent } from './compon_mk/page-details/page-details.component';
+import { NotfoundComponent } from './compon_mk/notfound/notfound.component';
 
 export const routes: Routes = [
 
     {path:'',component:MainPageComponent},
     {path:'details',component:PageDetailsComponent},
     {path:'profile',component:ProfileComponent},
-    // { path: 'notfound', component: NotFoundComponent },
-    // { path: '**', redirectTo: '/notfound' }
+    { path: 'notfound', component: NotfoundComponent },
+    { path: '**', redirectTo: '/notfound' }
 
 ];
