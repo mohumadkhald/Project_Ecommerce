f3a99419622765e196afaab332de656de0c6ba63
mohumadkhald <mohumadkhald@gmail.com>
2024-02-27 16:29:31 +0200

    resolve111

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './compon_mk/profile/profile.component';
 // import { NotFoundComponent } from './compon_mk/not_found /not-found.component';
+//test
 import { MainPageComponent } from './components/mu-reda/main_page/main-page.component';
 import { PageDetailsComponent } from './compon_mk/page-details/page-details.component';
 
