5988de6267d0466a613cbc112eb77a5afc056b15
mohamedreda0 <prindermedo8@gmail.com>
2024-03-02 20:15:16 +0200

    edit a lot of comps

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/mu-reda/header/header.component';
-import { NavbarComponent } from './components/mu-reda/navbar/navbar.component';
-import { SideBarComponent } from './components/mu-reda/side-bar/side-bar.component';
-import { SideBarSlidersComponent } from './components/mu-reda/side-bar-sliders/side-bar-sliders.component';
-import { FooterComponent } from './components/mu-reda/footer/footer.component';
+import { HeaderComponent } from './components/mu-reda/main-page-comps/header/header.component';
+import { NavbarComponent } from './components/mu-reda/main-page-comps/navbar/navbar.component';
+import { SideBarComponent } from './components/mu-reda/main-page-comps/side-bar/side-bar.component';
+import { SideBarSlidersComponent } from './components/mu-reda/main-page-comps/side-bar-sliders/side-bar-sliders.component';
+import { FooterComponent } from './components/mu-reda/main-page-comps/footer/footer.component';
 
 @Component({
   selector: 'app-root',
