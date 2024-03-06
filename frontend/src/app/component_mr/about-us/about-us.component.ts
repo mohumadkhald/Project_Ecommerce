7d6c7ba23c6edc66d6280c8c5ba583cd6d023a3a
mohamedreda0 <prindermedo8@gmail.com>
2024-03-07 00:14:33 +0200

    add login

---
+++ frontend/src/app/component_mr/about-us/about-us.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {StyleClassModule} from 'primeng/styleclass';
+
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [StyleClassModule],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
