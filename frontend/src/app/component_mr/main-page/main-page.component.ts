484b711e54ea5430a2a47cc216fe269b51c4ff38
mohamedreda0 <prindermedo8@gmail.com>
2024-03-07 07:50:22 +0200

    add card and products and side bar

--- frontend/src/app/component_mr/main-page/main-page.component.ts
+++ frontend/src/app/component_mr/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports:[HeaderComponent,SidebarComponent],
+  imports:[HeaderComponent,SidebarComponent,SidebarComponent,ProductsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
