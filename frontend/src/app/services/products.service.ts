3eafc3db2173574b171bed43c667dcf1f08d3ffd
zain <omarzain326@gmail.com>
2024-03-10 03:26:17 +0200

    what

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -9,7 +9,7 @@ export class ProductsService {
   constructor(private http: HttpClient) { }
 
   getProductsList(){
-    return this.http.get('http://127.0.0.1:8000/api/categories/index')
+    return this.http.get('http://127.0.0.1:8000/api/products')
   }
 
   getProductsDetails(id : number){
